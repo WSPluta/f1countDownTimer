3767bf6c46bec65fe4d4a0c9bb41098520627e31
jabrock <john.brock@oracle.com>
2023-02-24 12:01:00 -0800

    layout cleanups

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -8,7 +8,9 @@ import "ojs/ojformlayout";
 import "ojs/ojinputtext";
 
 export function Content() {
-  const [eventTime, setEventTime] = useState<Date>(new Date('02/24/2023 19:50:59'));
+  const [eventTime, setEventTime] = useState<Date>(
+    new Date("02/24/2023 19:50:59")
+  );
   const [name, setName] = useState<string>("Event Name");
   const [endOpened, setEndOpened] = useState<boolean>(false);
 
@@ -21,48 +23,74 @@ export function Content() {
   };
 
   return (
-    <div class="oj-sm-12">
-    <div class="oj-flex oj-sm-flex-direction-column oj-sm-3">
-      <div class="oj-flex-item oj-flex-bar oj-sm-align-items-center">
-        <div
-          role="img"
-          class="oj-flex-bar-start oj-icon orbr-oracle-icon oj-sm-align-items-centre"
-          title="red bull racing oracle partner logo"
-          alt="Red Bull Racing Oracle Partner"
-        ></div>
-      </div>
-      <div class="oj-flex-bar-middle oj-sm-flex-items-initial oj-sm-justify-content-center oj-sm-align-items-center">
+    // outer container
+    <div class="oj-flex oj-sm-flex-direction-row oj-sm-12 orbr-content-container">
+      {/* 3 column panel for times and event data */}
+      <div class="oj-flex-item oj-flex oj-sm-flex-direction-column oj-sm-4 orbr-event-panel">
+        <div class="oj-flex-item oj-sm-align-items-center">
+          <div
+            role="img"
+            class="oj-flex oj-flex-item oj-icon orbr-oracle-icon oj-sm-align-items-centre"
+            title="red bull racing oracle partner logo"
+            alt="Red Bull Racing Oracle Partner"
+          ></div>
+        </div>      
+        <div class="oj-flex-item oj-sm-flex-items-initial oj-sm-align-items-center orbr-clock-container">
+          Time stuff goes here
+        </div>
+        <div class="oj-flex-item oj-sm-flex-items-initial oj-sm-align-items-center orbr-event-container">
           <Event eventName={name} />
         </div>
-      <div class="oj-flex oj-sm-flex-items-initial oj-sm-align-items-centre orbr-counter">
-        <Counter targetTime={eventTime} />
+        <div class="oj-flex-item oj-sm-flex-items-initial oj-sm-align-items-centre orbr-counter-container">
+          <Counter targetTime={eventTime} />
+        </div>
       </div>
+
+      {/* 9 column panel for video or other content */}
+      <div class="oj-flex-item oj-sm-8">
+        <div class="orbr-video-container">video goes there</div>
+      </div>
+
+      {/* event configuration drawer */}
       <span>
-        <oj-drawer-popup class="orbr-drawer-end" edge="end" opened={endOpened}>
-          <div class="orbr-drawer-header">
-            <h6 class="orbr-drawer-text-color">Address</h6>
-            <oj-button
-              id="endButtonCloser"
-              display="icons"
-              chroming="borderless"
-              class="orbr-drawer-text-color"
-              onojAction={endToggle}
-            >
-              <span slot="startIcon" class="oj-ux-ico-close orbr-drawer-text-color"></span>
-              Close
-            </oj-button>
-          </div>
-          <div class="demo-padding demo-form-container">
-            <oj-form-layout>
-              <oj-input-text aria-label="line1" value="Line 1" class="orbr-drawer-text-color"></oj-input-text>
-              <oj-input-text aria-label="line2" value="Line 2" class="orbr-drawer-text-color"></oj-input-text>
-            </oj-form-layout>
-          </div>
-        </oj-drawer-popup>
-      </span>
-      <oj-button onojAction={open} label="Edit Events"></oj-button>
-    </div>
-    <div class="oj-sm-9">video goes there</div>
+          <oj-drawer-popup
+            class="orbr-drawer-end"
+            edge="end"
+            opened={endOpened}
+          >
+            <div class="orbr-drawer-header">
+              <h6 class="orbr-drawer-text-color">Address</h6>
+              <oj-button
+                id="endButtonCloser"
+                display="icons"
+                chroming="borderless"
+                class="orbr-drawer-text-color"
+                onojAction={endToggle}
+              >
+                <span
+                  slot="startIcon"
+                  class="oj-ux-ico-close orbr-drawer-text-color"
+                ></span>
+                Close
+              </oj-button>
+            </div>
+            <div class="demo-padding demo-form-container">
+              <oj-form-layout>
+                <oj-input-text
+                  aria-label="line1"
+                  value="Line 1"
+                  class="orbr-drawer-text-color"
+                ></oj-input-text>
+                <oj-input-text
+                  aria-label="line2"
+                  value="Line 2"
+                  class="orbr-drawer-text-color"
+                ></oj-input-text>
+              </oj-form-layout>
+            </div>
+          </oj-drawer-popup>
+        </span>
+        <oj-button onojAction={open} label="Edit Events"></oj-button>
     </div>
   );
 }
